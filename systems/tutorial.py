f6087b1a845586423bf90c15dad125b63bc73f1a
badsynthesis <git@badsynthesis.com>
2020-05-02 20:42:29 +0200

    Tutorial expansion

--- systems/tutorial.py
+++ systems/tutorial.py
@@ -41,6 +41,8 @@ class Tutorial:
         third_pos = Pos(second_pos.x + 1, second_pos.y)
         fourth_pos = Pos(third_pos.x + 1, third_pos.y)
 
+        linediff = 30
+
         if len(game_data.stats.moves) == 1 and game_data.player.pos != second_pos:
             Tutorial.TUTORIAL_SKIPPED = True  # player wanted to skip tutorial
             return messages
@@ -58,31 +60,21 @@ class Tutorial:
             )
             x = 20
             y = 370
-            diff = 30
             messages.append(Message("Use W,A,S,D to move", Pos(x, y), MessageType.LINE))
-            y += diff
             messages.append(
-                Message("You can also left-click on explored tiles to move there", Pos(x, y), MessageType.LINE,)
+                Message("You can also left-click on explored tiles to move there", Pos(x, y+linediff), MessageType.LINE,)
             )
-            y += diff
-            messages.append(Message("Press Tab for help", Pos(x, y), MessageType.LINE))
-            y += diff
-            messages.append(Message("Press Escape to quit", Pos(x, y), MessageType.LINE))
-            #messages.append(Message("Press Alt-Enter for fullscreen", Pos(20, 460), MessageType.LINE))
-            # text = "These are your formulas. You will gain more formulas, slots and ingredients as you level up"
-            # messages.append(Message(text, Pos(20, 200), MessageType.LINE, Size(200, 150)))
+            messages.append(Message("Press Tab for help", Pos(x, y+linediff*2), MessageType.LINE))
+            messages.append(Message("Press Escape to quit", Pos(x, y+linediff*3), MessageType.LINE))
+            if config.conf.crafting:
+                messages.append(Message("V to open crafting", Pos(x, y+linediff*4), MessageType.LINE))
+            if config.conf.consumables:
+                messages.append(Message("I to open invetory", Pos(x, y+linediff*5), MessageType.LINE))
 
-            # pygame.draw.rect(help_surface, colors.BACKGROUND, pygame.rect.Rect(20, 200, 200, 150))
-
-            # lines = textwrap.wrap(text, 20)
-            # display_lines(help_surface, assets.font_message, lines, 25, 205)
-
-            # move_px, move_py = px, py + 40
-            # messages.append(Message("Use W,A,S,D to move", Pos(move_px, move_py), MessageType.LINE))
 
         elif game_data.player.pos == second_pos:
             messages.append(Message("<--- This is your health bar", Pos(0, 20), MessageType.LINE))
-            messages.append(Message("<--- This is your shield bar", Pos(0, 70), MessageType.LINE))
+            messages.append(Message("<--- This is your shield bar, your shield will reset after this level", Pos(0, 70), MessageType.LINE))
             if not config.conf.keys:
                 messages.append(Message("This is your experience bar", Pos(20, 140), MessageType.LINE))
             else:
@@ -93,43 +85,51 @@ class Tutorial:
                         MessageType.LINE,
                     )
                 )
+            if config.conf.consumables:
+                y = 600
+                messages.append(Message("Down to the left are consumables, single use items that can help", Pos(0, y), MessageType.LINE))
+                messages.append(Message("You can mouse over for more info", Pos(20, y + linediff), MessageType.LINE))
 
         elif game_data.player.pos == third_pos:
+            x = 20
             cast_px, cast_py = px, py + 40
             messages.append(Message("Press 1,2 or 3 to select vial", Pos(cast_px, cast_py), MessageType.LINE,))
             messages.append(
-                Message("FFR is Fire, Fire, Range. Short range, high damage", Pos(20, 220), MessageType.LINE,)
+                Message("FFR is Fire, Fire, Range. Short range, high damage", Pos(x, 220), MessageType.LINE,)
             )
             messages.append(
-                Message("FRR is Fire, Range, Range. Longer range, lower damage", Pos(20, 260), MessageType.LINE,)
+                Message("FRR is Fire, Range, Range. Longer range, lower damage", Pos(x, 260), MessageType.LINE,)
             )
             messages.append(
                 Message(
-                    "This is a shield, activate it and it will protect you from damage", Pos(20, 300), MessageType.LINE,
+                    "This is a shield, activate it and it will protect you from damage", Pos(x, 300), MessageType.LINE,
                 )
             )
             messages.append(
                 Message(
-                    "You can also melee attack enemies by moving into them, this won't deal much damage though", Pos(80, 360), MessageType.LINE,
+                    "You can also melee attack enemies by moving into them, this won't deal much damage though", Pos(x, 360), MessageType.LINE,
                 )
             )
 
         elif game_data.player.pos == fourth_pos:
-            messages.append(Message("This first enemy is inactive, others won't be.", Pos(80, 160), MessageType.LINE,))
-            messages.append(
-                Message("Kill it the second Formula, FRR", Pos(80, 200), MessageType.LINE,)
-            )
-            messages.append(Message("Press 2, or click the formula, to target", Pos(80, 240), MessageType.LINE,))
+            x = 80
+            y = 160
 
-            messages.append(Message("Defensive formulas will always target yourself", Pos(80, 300), MessageType.LINE,))
+            messages.append(Message("This first enemy is inactive, others won't be.", Pos(x, y), MessageType.LINE,))
+            messages.append(
+                Message("Kill it the second Formula, FRR", Pos(x, y + linediff), MessageType.LINE,)
+            )
+            messages.append(Message("Press 2, or click the formula, to target", Pos(x, y + linediff*2), MessageType.LINE,))
+
+            messages.append(Message("Defensive formulas will always target yourself", Pos(x, y + linediff*3), MessageType.LINE,))
 
         elif game_data.player.pos == Pos(14,5):
             x = 20
             y = 200
-            diff = 30
+
             messages.append(Message("This is a key, you need these to unlock stairs", Pos(x, y), MessageType.LINE))
-            messages.append(Message("Move onto it and press space to pick it up", Pos(x, y + diff), MessageType.LINE))
-            messages.append(Message("When you do, you'll get a little loot", Pos(x, y + 2*diff), MessageType.LINE))
+            messages.append(Message("Move onto it and press space to pick it up", Pos(x, y + linediff), MessageType.LINE))
+            messages.append(Message("When you do, you'll get a little loot", Pos(x, y + linediff*2), MessageType.LINE))
 
         elif game_data.player.pos == Pos(10,5):
             for e in game_data.map.entities:
@@ -139,9 +139,10 @@ class Tutorial:
                     messages.append(Message("Stairs, press Space to ascend", Pos(sx, sy), MessageType.LINE))
 
                     messages.append(Message("When you ascend:", Pos(sx, sy+50), MessageType.LINE))
-                    messages.append(Message("1) You will get to change your formulas", Pos(sx+30, sy+75), MessageType.LINE))
-                    messages.append(Message("2) Your cooldowns will reset", Pos(sx+30, sy+100), MessageType.LINE))
-                    messages.append(Message("3) Your shield will remain", Pos(sx+30, sy+115), MessageType.LINE))
+                    sy += 75
+                    messages.append(Message("1) You will get to change your formulas", Pos(sx+30, sy), MessageType.LINE))
+                    messages.append(Message("2) Your cooldowns will reset", Pos(sx+30, sy+linediff), MessageType.LINE))
+                    messages.append(Message("3) Your shield will remain", Pos(sx+30, sy+linediff*2), MessageType.LINE))
                     break
 
         else:
@@ -155,7 +156,7 @@ class Tutorial:
                 if not config.conf.keys:
                     messages.append(Message("Go to the corpse and press space to loot", Pos(mx, my), MessageType.LINE,))
                 messages.append(Message("Now your formula is on cooldown", Pos(20, 220), MessageType.LINE))
-                messages.append(Message("Cooldown reduce when you explore new tiles", Pos(20, 260), MessageType.LINE,))
+                messages.append(Message("Cooldown reduce when you explore new tiles", Pos(20, 220+linediff), MessageType.LINE,))
 
         if game_data.state == GameStates.TARGETING:
             target_px, target_py = px, py + 40
